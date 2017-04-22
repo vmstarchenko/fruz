f4d8380715bf65d04d32c3a8b02719fe4830fc71
vmstarchenko <vmstarchenko@edu.hse.ru>
2017-04-22 20:18:49 +0300

    add cron

--- src/apps/schedule/scripts/clock.py
+++ src/apps/schedule/scripts/clock.py
@@ -4,8 +4,8 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from schedule.tools import update_periods_database
 
 def update_periods_database_handler():
-    print('update')
-    # update_periods_database()
+    # TODO: log results
+    update_periods_database()
 
 
 def run():
