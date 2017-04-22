388b873aaec3434230e8d994de80ae121f20df33
vmstarchenko <vmstarchenko@edu.hse.ru>
2017-04-22 15:59:20 +0300

    prod

--- src/core/settings/prod.py
+++ src/core/settings/prod.py
@@ -9,10 +9,10 @@ ALLOWED_HOSTS = ALLOWED_HOSTS + ['fruz.herokuapp.com',]
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django_db',
-        'USER': 'admin_me',
-        'PASSWORD': 'adminadminquerty',
-        'HOST': 'fruz.herokuapp.com',
+        'NAME': 'd2r5al0ttce33e',
+        'USER': 'kcnqsgijwlvrax',
+        'PASSWORD': 'be653ac5064d5713b8b467bbf7016bddd65406345ce11c2e62843ac29df48cd4',
+        'HOST': 'ec2-79-125-125-97.eu-west-1.compute.amazonaws.com',
         'PORT': '5432',
 
     }
